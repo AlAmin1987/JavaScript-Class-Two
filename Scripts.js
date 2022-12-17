57a5a606e8be4cb420d780dda058b9b4ab931ad7
Md.AlAmin <alamin.dreams2021@gmail.com>
2022-12-17 23:54:08 +0600

    first commit

---
+++ Scripts.js
@@ -0,0 +1,121 @@
+//let firstName = "Asif";
+//console.log(firstName);
+//let age = 33;
+//console.log(age);
+
+/*datatype
+1 Object
+2 Premative
+---------------
+
+premative Data Type :
+1.Number
+2.String
+3.Boolean
+_______________
+4.Undefine
+5.Null
+6.Symble
+7.BigInt
+*/
+
+//Number
+let age = 30;
+console.log(age);
+
+//String
+let firstName = "Asif";
+console.log(firstName);
+
+//Boolean
+let isSinger = "True";
+console.log(isSinger);
+
+//Undefine
+
+let homeCity;
+console.log(homeCity);
+
+// Null
+let income = null;
+// console.log(income);
+// console.log(typeof homeCity);
+// console.log(typeof firstName);
+
+// homeCity = "Dhaka";
+// console.log(homeCity);
+// console.log(isSinger);
+// isSinger = "Bangladeshi Singer";
+// console.log(isSinger);
+// console.log(typeof isSinger);
+/* let
+   const
+   var
+   */
+// let myAge = 39;
+// console.log(myAge);
+// myAge = 45;
+// console.log(myAge);
+
+// let currentYear = 2022;
+// let myAge = currentYear - 1987;
+
+// console.log(myAge);
+
+// let janntulMawaAge = currentYear - 2016;
+// console.log(janntulMawaAge);
+
+// console.log(myAge, janntulMawaAge);
+
+// let ageDiff = myAge - janntulMawaAge;
+// console.log(ageDiff);
+
+console.log(10 + 5);
+console.log(28 - 6);
+console.log(25 * 3);
+console.log(9 ** 3);
+console.log(25 % 7);
+
+// let x = 15;
+// x++, console.log(x);
+
+// let y = 69;
+// y--, console.log(y);
+
+firstName = "Asif";
+lastName = "Akbar";
+
+console.log(firstName + lastName);
+
+console.log(firstName + " " + lastName);
+
+let x = 6;
+x += 10;
+console.log(x);
+
+let y = 75;
+y -= 45;
+
+console.log(y);
+
+y = 35;
+(y /= 5), console.log(y);
+
+x = 98;
+x %= 15;
+
+console.log(x);
+
+let z = 128;
+let s = 75;
+
+console.log((z /= s));
+
+y = 69;
+y %= 6;
+console.log(y);
+
+x = 45;
+x **= 5;
+
+console.log(x);
